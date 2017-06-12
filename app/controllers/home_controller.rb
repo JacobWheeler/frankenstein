ddf8eda6ad2d1bba5e83bc3f515607da03909b2e
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-06-12 13:31:16 -0600

    log in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,21 +1,21 @@
 class HomeController < ApplicationController
-  before_action :set_movie
+
   
   def index
     @movies = current_user.movies.all
   end
 
-   def add_to_library
-    movie = Movie.find(params[:id])
-    library = current_user.library
-    library.movies << movie
-    library.save
-    redirect_to @library
-  end
+  #  def add_to_library
+  #   movie = Movie.find(params[:id])
+  #   library = current_user.library
+  #   library.movies << movie
+  #   library.save
+  #   redirect_to @library
+  # end
 
   private
-   def set_movie
+   def set_library
     @movie = Movie.find(params[:id])
-  end
+   end
 
 end
