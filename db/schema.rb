b3ab8f258bb62770c68cce9b9a1b4e8da3f714d0
Scott Moore <scotchymoore@gmail.com>
2017-06-12 17:06:40 -0600

    log in changes

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 20170610161151) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "first_name", null: false
+    t.string "first_name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
