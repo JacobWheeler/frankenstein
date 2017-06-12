c93ed10474f5d163ca6629f040dfa4be523e7305
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-06-12 16:36:26 -0600

    random changes

--- spec/models/library_spec.rb
+++ spec/models/library_spec.rb
@@ -1,7 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Library, type: :model do
+<<<<<<< HEAD
 	describe 'validations' do
 		it { should validate_presence_of (:title) }
 	end
+=======
+  describe 'validation' do
+    it {should validate_presence_of(:title)}
+  end
+  
+>>>>>>> random changes
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+<<<<<<< HEAD
 
 	describe 'validations' do
 		it { should validate_presence_of (:first_name) }
 	end
+=======
+  describe 'validation' do
+    it {should validate_presence_of(:first_name)}
+  end
+>>>>>>> random changes
 end
