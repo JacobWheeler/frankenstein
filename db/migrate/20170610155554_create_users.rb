6cf18ba0959441fd00db4057d55dc3655bcfced7
Jacob Wheeler <wheeler.jacob.r@gmail.com>
2017-06-13 20:40:46 -0600

    db changes

--- db/migrate/20170610155554_create_users.rb
+++ db/migrate/20170610155554_create_users.rb
@@ -1,10 +1,6 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
-<<<<<<< HEAD
-      t.string :first_name
-=======
->>>>>>> not sure
       t.integer :age
 
       t.timestamps
